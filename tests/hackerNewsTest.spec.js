69d8d83d3a537d9c657d01304340f0642f7f6a25
Katya <asilkatya@gmail.com>
2025-02-28 13:06:24 +0000

    backup, moving to the nextt page and checking timestamps

--- tests/hackerNewsTest.spec.js
+++ tests/hackerNewsTest.spec.js
@@ -1,28 +1,36 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('100 articles sorted', async ({ page }) => {
-  await page.goto('https://news.ycombinator.com/newest');
+test("100 articles sorted", async ({ page }) => {
+  await page.goto("https://news.ycombinator.com/newest");
 
-  const moreButton = page.getByRole('link', {name: 'More'});
+  const moreButton = page.getByRole("link", { name: "More" });
 
   // Test for cell 1 to contain text '1'
-  page.getByRole('cell', { name: '1' });
-  await expect(page.getByRole('cell', { name: '1.', exact: true })).toHaveText('1.');
+  page.getByRole("cell", { name: "1" });
+  await expect(page.getByRole("cell", { name: "1.", exact: true })).toHaveText(
+    "1."
+  );
 
   // Expect 30 articles on the first page
-  const articles = await page.locator('.athing').count();
+  const articles = await page.locator(".athing").count();
   expect(articles).toBe(30);
 
   // Check the articles go from newest to oldest
-  expect (await page.locator('.age').count()).toBe(30);
+  expect(await page.locator(".age").count()).toBe(30);
 
   // Extracting article timecodes from the first page
-  const times1 = await page.locator('.age').evaluateAll((ages) =>
-    ages.map(age => age.getAttribute('title'))
-  );
+  const times1 = await page
+    .locator(".age")
+    .evaluateAll((ages) => ages.map((age) => age.getAttribute("title")));
 
-  expect(times1.length).toBe(30);
-
+  // Only keeping timestamps, slicing out the rest of the timecode
+  const timeStamps1 = times1.map((time) => time.slice(20));
+  // Rearranging timestamps in the descending order
+  const sortedTimeStamps1 = [...timeStamps1].sort((a, b) => b - a);
+  expect(timeStamps1).toEqual(sortedTimeStamps1);
   await moreButton.click();
+  const times2 = await page
+    .locator(".age")
+    .evaluateAll((ages) => ages.map((age) => age.getAttribute("title")));
+  console.log(times2);
 });
-
